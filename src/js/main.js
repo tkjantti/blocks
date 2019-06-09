8762045824fba7c1e617348bb14ce18686fbbf19
Tero Jäntti <tkjantti@gmail.com>
2019-06-09 14:01:44 +0300

    Filling the grid with blocks of different color.

--- src/js/main.js
+++ src/js/main.js
@@ -14,11 +14,31 @@ const ySquareCount = Math.floor(canvas.height / squareHeight);
 let deltaTimeMs = 0;
 let lastTimeStampMs = 0;
 
+const BLOCK_RED = 1;
+const BLOCK_YELLOW = 2;
+const BLOCK_GREEN = 3;
+
+const blocks = [xSquareCount * ySquareCount];
+
+
+function getColor(block, x, y) {
+  switch (block) {
+  case BLOCK_RED:
+    return 'red';
+  case BLOCK_YELLOW:
+    return 'yellow';
+  case BLOCK_GREEN:
+    return 'green';
+  default:
+    return `hsl(200, ${40 + ((x + y) % 2) * 30}%, 20%)`;
+  }
+}
 
 function drawGrid() {
-  for (let y = 0; y < ySquareCount; y++) {
-    for (let x = 0; x < xSquareCount; x++) {
-      ctx.fillStyle = `hsl(200, ${40 + ((x + y) % 2) * 30}%, 20%)`;
+  for (let x = 0; x < xSquareCount; x++) {
+    for (let y = 0; y < ySquareCount; y++) {
+      const block = blocks[x * ySquareCount + y];
+      ctx.fillStyle = getColor(block, x, y);
       ctx.fillRect(x * squareWidth, y * squareHeight, squareWidth, squareHeight);
     }
   }
@@ -33,4 +53,16 @@ function mainLoop(ms) {
   drawGrid();
 }
 
-requestAnimationFrame(mainLoop);
+function initializeGame() {
+  const blockTypes = [BLOCK_RED, BLOCK_YELLOW, BLOCK_GREEN];
+
+  for (let x = 0; x < xSquareCount; x++) {
+    for (let y = 0; y < ySquareCount; y++) {
+      blocks[x * ySquareCount + y] = blockTypes[Math.floor(Math.random() * blockTypes.length)];
+    }
+  }
+
+  requestAnimationFrame(mainLoop);
+}
+
+initializeGame();
