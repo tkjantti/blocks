5420e871a4daf6d95b4fb81d6378d5a919a3d114
Tero Jäntti <tkjantti@gmail.com>
2019-08-05 16:27:28 +0300

    Webpack used instead of budo.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/* jshint node:true */
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Blocks',
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true,
+        minifyCSS: true,
+        removeComments: true,
+      }
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
