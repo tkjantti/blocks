97f80118561b59ca513b25700aba74c148095c09
Tero Jäntti <tkjantti@gmail.com>
2020-01-11 19:53:15 +0200

    Refactoring new class Level.

--- src/main.js
+++ src/main.js
@@ -22,70 +22,17 @@
  * SOFTWARE.
  */
 
-import Grid from "./Grid.js";
-import {
-  squareWidth,
-  squareHeight,
-  canvas,
-  drawGrid,
-  drawScore
-} from "./Graphics.js";
+import { canvas } from "./Graphics.js";
 import { listenMouseClicks } from "./Controls.js";
-import AnimationState from "./AnimationState.js";
-import { BLOCK_NONE, BLOCK_RED, BLOCK_YELLOW, BLOCK_GREEN } from "./Block.js";
+import { Level } from "./Level.js";
 
 const TIME_STEP = 1000 / 60;
 const MAX_FRAME = TIME_STEP * 5;
 
-const ANIM_SPEED = 100;
-
-const xSquareCount = Math.floor(canvas.width / squareWidth);
-const ySquareCount = Math.floor(canvas.height / squareHeight);
-
 let deltaTimeMs = 0;
 let lastTimeStampMs = 0;
 
-const grid = new Grid(xSquareCount, ySquareCount, BLOCK_NONE);
-
-let shiftDownTimeElapsed = 0;
-let topShiftDownCount = 0;
-
-let shiftLeftTimeElapsed = 0;
-let topShiftLeftCount = 0;
-
-let animState = new AnimationState();
-
-let score = 0;
-
-function animateBlocks(deltaTimeMs) {
-  if (topShiftDownCount > 0) {
-    shiftDownTimeElapsed += deltaTimeMs;
-    const shiftDownRatio = shiftDownTimeElapsed / ANIM_SPEED;
-
-    if (shiftDownRatio >= topShiftDownCount) {
-      shiftDownTimeElapsed = 0;
-      topShiftDownCount = 0;
-      grid.resetVerticalPositions();
-
-      topShiftLeftCount = grid.shiftBlocksLeft();
-    }
-
-    animState.shiftDownRatio = shiftDownRatio;
-  } else if (topShiftLeftCount > 0) {
-    shiftLeftTimeElapsed += deltaTimeMs;
-    const shiftLeftRatio = shiftLeftTimeElapsed / ANIM_SPEED;
-
-    if (shiftLeftRatio >= topShiftLeftCount) {
-      shiftLeftTimeElapsed = 0;
-      topShiftLeftCount = 0;
-      grid.resetHorizontalPositions();
-    }
-
-    animState.shiftLeftRatio = shiftLeftRatio;
-  }
-
-  return animState;
-}
+const level = new Level();
 
 function gameLoop(ms) {
   requestAnimationFrame(gameLoop);
@@ -93,49 +40,14 @@ function gameLoop(ms) {
   deltaTimeMs = Math.min(ms - lastTimeStampMs, MAX_FRAME);
   lastTimeStampMs = ms;
 
-  animateBlocks(deltaTimeMs);
+  level.update(deltaTimeMs);
 
-  drawGrid(grid, animState);
-
-  drawScore(score);
-}
-
-function calculateScore(count) {
-  return count * count;
+  level.draw();
 }
 
 function initializeGame() {
-  const blockTypes = [BLOCK_RED, BLOCK_YELLOW, BLOCK_GREEN];
-  grid.initialize(
-    () => blockTypes[Math.floor(Math.random() * blockTypes.length)]
-  );
-
-  listenMouseClicks(canvas, function(mouseX, mouseY) {
-    if (topShiftDownCount > 0 || topShiftLeftCount > 0) {
-      return;
-    }
-
-    const x = Math.floor(mouseX / squareWidth);
-    const y = Math.floor(mouseY / squareHeight);
-
-    if (!grid.isContiguousArea(x, y)) {
-      return;
-    }
-
-    const count = grid.clearContiguousBlocks(x, y);
-    score += calculateScore(count);
-
-    topShiftDownCount = grid.shiftBlocksDown();
-    if (topShiftDownCount === 0) {
-      // No need to animate dropping down, start shift left animation immediately.
-      topShiftLeftCount = grid.shiftBlocksLeft();
-    }
-  });
+  listenMouseClicks(canvas, level.onClick.bind(level));
 }
 
-function main() {
-  initializeGame();
-  requestAnimationFrame(gameLoop);
-}
-
-main();
+initializeGame();
+requestAnimationFrame(gameLoop);
